4c17cbcd8ca31b3d0278e8e4f850c5c175b46ebc
K-Kumar-01 <59891164+K-Kumar-01@users.noreply.github.com>
2020-09-04 19:43:59 +0530

    order validator added

--- routers/orders.js
+++ routers/orders.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { getAllOrders, createOrder } = require('../controllers/orders');
+const { orderCheckValidator } = require('../validators/shop');
+const { runValidation } = require('../validators');
 
 const router = express.Router();
 
 router.get('/', getAllOrders);
-router.post('/create', createOrder);
+router.post('/create', orderCheckValidator, runValidation, createOrder);
 
 module.exports = router;
--- validators/shop.js
+++ validators/shop.js
@@ -25,3 +25,16 @@ exports.productCreationValidator = [
 exports.productUpdationValidator = [
 	check('quantity').not().isEmpty().isInt({ min: 0 }).withMessage('Quantity entered is invalid'),
 ];
+
+exports.orderCheckValidator = [
+	check('email').isEmail().normalizeEmail().not().isEmpty().withMessage('Must have a email'),
+	check('address').not().isEmpty().withMessage('Need address to ship'),
+	check('city').not().isEmpty().withMessage('City required'),
+	check('postalcode').not().isEmpty().withMessage('Need postalcode'),
+	check('country').not().isEmpty().withMessage('Country required'),
+	check('cardname').not().isEmpty().withMessage('Card holder name required'),
+	check('ccnumber').not().isEmpty().withMessage('Card Number field required'),
+	check('expdate').not().isEmpty().withMessage('Expiry date of card is required'),
+	check('cvv').not().isEmpty().withMessage('CVV cannot be empty'),
+	check('items').not().isEmpty().withMessage('Atleast one product must be there'),
+];
